bd7966f808b2dcd652926f8443475b57248c361f
vagisha312 <108082732+vagisha312@users.noreply.github.com>
2024-03-13 18:37:45 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,6 +1,3 @@
 
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-    key: "",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
+
+
